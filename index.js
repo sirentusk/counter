5d28b2d8b7ade3ff5a1cef1a70565fb2370e3f72
Siren <143248933+sirentusk@users.noreply.github.com>
2024-04-07 13:37:10 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const decrease = document.getElementById("decrease");
-const reset = document.getElementById("reset");
-const increase = document.getElementById("increase");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
+const decrease = document.getElementById("decrease"); // Declare container
+const reset = document.getElementById("reset"); // Declare container
+const increase = document.getElementById("increase"); // Declare container
+const countLabel = document.getElementById("countLabel"); // Declare container
+let count = 0; // Declare container "count"
 
-increase.onclick = function(){
-    count++;
-    countLabel.textContent = count;
+increase.onclick = function(){ // Record the click
+    count++; // Add by +1
+    countLabel.textContent = count; // Change the text content in container "count"
 }
 
-decrease.onclick = function(){
-    count--;
-    countLabel.textContent = count;
+decrease.onclick = function(){ // Record the click
+    count--; // Minus by -1
+    countLabel.textContent = count; // Change the text content in container "count"
 }
 
-reset.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
+reset.onclick = function(){ // Record the click
+    count = 0; // Return to 0
+    countLabel.textContent = count; // Change the text content in container "count"
 }
